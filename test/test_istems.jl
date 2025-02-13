7b44477befa5a20022fe8341a0dfd34fe58e66db
Neel Smith <dnsmith.neel@gmail.com>
2025-02-12 21:08:48 -0500

    Clean up some noun data

---
+++ test/test_istems.jl
@@ -0,0 +1,17 @@
+@testset "Test parsing CGPT summaries for 3rd declension istems" begin
+    # animal
+    # arx
+    # cŭbīle
+    # ebur
+    # far
+    # frater
+    # fulgur
+    # glans
+    # guttur
+    # jecur 
+    # lăquĕar
+    # mater
+    # pater
+    # renes
+    # cicer
+end
